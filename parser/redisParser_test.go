4654aed283bc84d256bbdc29bf05b34d0bac2f60
dbb2xbb <root@dbb2xbb>
2019-06-23 16:52:36 +0800

    去掉测试用例中的无用字段

--- parser/redisParser_test.go
+++ parser/redisParser_test.go
@@ -5,19 +5,18 @@ import (
 	"testing"
 )
 
-var tests = []struct{
+var tests = []struct {
 	TestStr string
-	result string
 }{
-	{`*3\r\n$3\r\nSET\r\n$-1\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n`, 	""},
-	{`*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n`, 	""},
-	{`+OK\r\n`, 	""},
-	{`:1000\r\n`, ""},
-	{`-ERR unknown command 'foobar'\r\n`,""},
-	{`-WRONGTYPE Operation against a key holding the wrong kind of value\r\n`,""},
-	{`*3\r\n$-1\r\n$2\r\nHI\r\n`,""},
-	{`$5\r\nHello\r\n`,""},
-	{`$-1\r\n`,""},
+	{`*3\r\n$3\r\nSET\r\n$-1\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n`},
+	{`*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n`},
+	{`+OK\r\n`},
+	{`:1000\r\n`},
+	{`-ERR unknown command 'foobar'\r\n`},
+	{`-WRONGTYPE Operation against a key holding the wrong kind of value\r\n`},
+	{`*3\r\n$-1\r\n$2\r\nHI\r\n`},
+	{`$5\r\nHello\r\n`},
+	{`$-1\r\n`},
 }
 
 func TestRedisParser(t *testing.T) {
@@ -25,6 +24,3 @@ func TestRedisParser(t *testing.T) {
 		fmt.Println(RedisParser(test.TestStr))
 	}
 }
-
-
-
